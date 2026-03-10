6fa1c3501c153c0914aca54e8ae84e75e368fd95
jiri132 <jiri28beurskens@gmail.com>
2026-03-10 16:43:05 +0100

    Updated configs to include hidden dimension

--- src/config.py
+++ src/config.py
@@ -1,15 +1,16 @@
 import torch
 
 # Global configuration for training and evaluation
-MODEL_NAME = "BiLSTM_Attention"
+MODEL_NAME = "Classifier_Attention_Bernoulli"
 
 # Training hyperparameters
 SEED = 42
-MAX_LEN = 25
-BATCH_SIZE = 32
-EMBED_DIM = 100
+MAX_LEN = 50
+BATCH_SIZE = 64
+EMBED_DIM = 250
+HIDDEN_DIM = 256
 LR = 3e-4
-EPOCHS = 6
+EPOCHS = 10
 
 # Data and checkpoint paths
 DATA_PATH = "data/train_binary.csv"
